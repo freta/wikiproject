1dfab3c879f4dd6c051691527d6bedf552988e9d
freta <[itsmefruta@gmail.com]>
2020-07-30 18:40:57 +0300

    Presentation of the poem page template

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 // $(function () {
 //   $('#datepicker').datapicker({ dateFormat: 'yy' });
 // });
-/***iframe */
+/***iframe 
 let myIframe = document.getElementById('myIframe');
 let url_string = 'home.html';
 myIframe.src = url_string;
-console.log(url_string);
+console.log(url_string);*/
+
+/*** lyric worder/prsentation */
+let x = document.getElementsByClassName('lyricOne');
+x.innerHTML = ' ፩) ';
